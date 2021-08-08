113919bc103ff8bdd7b01bdfd94499d7d58f7e94
Balazs9 <sempergerbalazs@gmail.com>
2021-08-08 20:45:28 +0000

    adding docstring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,56 @@
-let questions = [
-    {
-        question: "Which team won the World Cup in 1938?",
+// wait for the DOM to finish loading the game
+// get the button elements and add event listeners to the
 
-        answer: {
-            a: 'Italy',
-            b: 'Germany',
-            c: 'Spain',
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "Which player won the Ballon D'or in 2003?",
-
-        answer: {
-            a: 'Pavel Nedved',
-            b: 'Zinadine Zidane',
-            c: 'Ronaldo',
-        },
-        correctAnswer: 'a'
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute('question-container') === 'submit') {
+                checkAnswer();
+            }
+            else {
+                let questType = this.getAttribute("question-container");
+                startFootball(questType);
+            }
+        })
     }
-]
-let startButton = document.getElementById('start-btn');
-let nextButton = document.getElementById('next-btn');
-let questionContainer = document.getElementById('question-container');
-let questionElement = document.getElementById('questions')
-let answerElement = document.getElementById('answer-button')
-let shuffledQuestions = [];
-startButton.addEventListener('click', startButton);
-let showQuestions = questions.length;
+    document.getElementById('btn').addEventListener('keydown', function(event) {
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+    startFootball(question);
+},
+
+/**
+ * main game loop, called when the game script is loaded
+ * and after the gamer answers has been processed
+ */
+
 
 function startFootball() {
+    let startButton = document.getElementById('start-btn');
+    let nextButton = document.getElementById('next-btn');
+    let questionContainer = document.getElementById('question');
+    let questionElement = document.getElementById('questions');
     console.log('started');
     setNextQuestion();
 
 
-}
+},
 function handleQuestions (){
+    let shuffledQuestions = [];
     while (shuffledQuestions.length <= 20) {
         let random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)) {
             shuffledQuestions.push(random)
         }
     }
-}
+},
+
 function setNextQuestion(questions) {
     resetState();
+    let answerElement = document.getElementById('answer-buttons')
     questionElement.innerText = question.question;
     question.answers.forEach(answer => {
         let button = document.createElement('button');
@@ -59,22 +65,47 @@ function setNextQuestion(questions) {
         answerButtonsElement.appendChild(button);
     })
     
-}
+},
 
 function selectAnswer() {
 
-}
+},
 /**
  * gets the current score from DOM and increment by 1
  */
 function incrementScore() {
     let oldScore = parseInt(document.getElementById('score').innerText);
         document.getElementById('score').innerText = ++oldScore;    
-}
+},
 /**
  * gets the current incorrect answer from DOM and increment by 1
  */
 function incrementWrongAnswer(){
     let oldScore = parseInt(document.getElementById('incorrect').innerText);
         document.getElementById('incorrect').innerText = ++oldScore; 
-}
+},
+
+let questions = [
+    {
+        question: "Which team won the World Cup in 1938?",
+
+        answer: {
+            a: 'Italy',
+            b: 'Germany',
+            c: 'Spain',
+            d: 'France',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Which player won the Ballon D'or in 2003?",
+
+        answer: {
+            a: 'Pavel Nedved',
+            b: 'Zinadine Zidane',
+            c: 'Ronaldo',
+            d: 'Luis Figo'
+        },
+        correctAnswer: 'a'
+    }
+]
