20e3f2b323047623e9314fbd4e8cf938ce80ac0c
Balazs9 <sempergerbalazs@gmail.com>
2021-08-08 16:05:48 +0000

    Add score and incorrect function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+let questions = [
+    {
+        question: "Which team won the World Cup in 1938?",
+
+        answer: {
+            a: 'Italy',
+            b: 'Germany',
+            c: 'Spain',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Which player won the Ballon D'or in 2003?",
+
+        answer: {
+            a: 'Pavel Nedved',
+            b: 'Zinadine Zidane',
+            c: 'Ronaldo',
+        },
+        correctAnswer: 'a'
+    }
+]
 let startButton = document.getElementById('start-btn');
 let nextButton = document.getElementById('next-btn');
 let questionContainer = document.getElementById('question-container');
@@ -30,34 +52,21 @@ function setNextQuestion(questions) {
     })
     
 }
-function resetState() {
-    while (answerButtonsElement.firstChild {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
+
 function selectAnswer() {
 
 }
-
-let questions = [
-    {
-        question: "Which team won the World Cup in 1938?",
-
-        answer: {
-            a: 'Italy',
-            b: 'Germany',
-            c: 'Spain',
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "Which player won the Ballon D'or in 2003?",
-
-        answer: {
-            a: 'Pavel Nedved',
-            b: 'Zinadine Zidane',
-            c: 'Ronaldo',
-        },
-        correctAnswer: 'a'
-    }
-]
+/**
+ * gets the current score from DOM and increment by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+        document.getElementById('score').innerText = ++oldScore;    
+}
+/**
+ * gets the current incorrect answer from DOM and increment by 1
+ */
+function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+        document.getElementById('incorrect').innerText = ++oldScore; 
+}
