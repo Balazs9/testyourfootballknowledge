9a6c772cb2a5a5d7ade7cc02e1bbfb152f62393b
Balazs9 <sempergerbalazs@gmail.com>
2022-01-27 20:59:51 +0000

    add function to display question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const questionBox = document.getElementById('questions');
 const resultsBox = document.getElementById('answer-buttons');
 const submitButton = document.getElementById('submit');
-const questionNumber = 0;
 const scoreCorrect = document.getElementById("score");
 const scoreWrong = document.getElementById("incoreect"); 
 const answerBox = document.getElementById("answer");
@@ -9,13 +8,9 @@ const answer1 = document.getElementById("opt0");
 const answer2 = document.getElementById("opt1");
 const answer3 = document.getElementById("opt2");
 const answer4 = document.getElementById("opt3");
+const startingPage = 0;
 
-let questionNumb = "";
 
-questionBox.innerHTML = questionNumb;
-function start() {
-    
-}
 /*
 function game() {
     questionBox.innerText = question.quest;
@@ -32,23 +27,8 @@ function game() {
 
 }
 */
-let start = game();
-/*
-function containerQuiz (questionBox){
-    let currentQuest = quest.length;
-    while (currentQuest > 1) {
-    questionBox.innerHTML = Math.floor(Math.random()*quest.length);
-    currentQuest -= 1;
-    } return quest;
-};*/
-/* function name() {
-    var user = prompt("Please enter your name");
-} */
-/*function showResults(){}
-    containerQuiz();
+document.getElementById("submit").addEventListener("click", nextQuestion);
 
-    submitButton.addEventListener('click', showResults);
-*/
 /**
  * Displaying the quiz questions
  */
@@ -56,16 +36,23 @@ let question = [
     {
         quest: "Which team won the World Cup in 1938?",
 
-        answer: ['Italy', 'Germany', 'Spain', 'France'],
+        answers: ['Italy', 'Germany', 'Spain', 'France'],
         correctAnswer: 1
     },
     {
         quest: "Which player won the Ballon D'or in 2003?",
 
-        answer: ['Pavel Nedved', 'Zinadine Zidane', 'Ronaldo', 'Luis Figo'],
+        answers: ['Pavel Nedved', 'Zinadine Zidane', 'Ronaldo', 'Luis Figo'],
         correctAnswer: 1
     }
 ]
+console.log(question);
+function nextQuestion() {
+    questionBox.textContent = question[0].quest;
+    console.log(question);
+}
+nextQuestion();
+
 /*
 function showQuestion(){
     let randomQuestion = Math.floor(Math.random()*question.length);
@@ -81,13 +68,16 @@ showQuestion(question);
 /**
  * gets the current score from DOM and increment by 1
  */
+/*
  function incrementScore() {
     let oldScore = parseInt(document.getElementById('score').innerText);
         document.getElementById('score').innerText = ++oldScore;    
 }
+*/
 /**
  * gets the current incorrect answer from DOM and increment by 1
  */
+/*
 function incrementWrongAnswer(){
     let oldScore = parseInt(document.getElementById('incorrect').innerText);
         document.getElementById('incorrect').innerText = ++oldScore; 
@@ -97,6 +87,7 @@ function incrementWrongAnswer(){
 /**
  * ScoreBoard 
  */
+/*
 let results =[];
 results[score]= incrementScore() + incrementWrongAnswer();
 let i=0;
@@ -104,39 +95,12 @@ while (i<1000) {
     results.push(Math.floor(Math.random()*1000));
     i++;
 };
-
-let table = document.createElement('table');
-table.id = 'random-results';
-
-let tableHtml = `
-<thead>
-    <tr>
-        <td>Players #</td>
-        <td>Points</td>
-    </tr>
-</thead>
-<tbody>
-</tbody>
-`;
-
-table.innerHtml = tableHtml;
-let tbody = table.getElementsByTagName('tbody')[0];
-let tbodyHtml = '';
-for (let i=0; i<results.length; i++) {
-    tbodyHtml += `
-            <tr>
-                <td>&{i}</td>
-                <td>&{results[i]}</td>
-            </tr>
-            `
-}
-tbody.innerHtml = tbodyHtml;
-document.body.appendChild(table);
+*/
 
 /**
  * sign-in 
  */
-
+/*
 function handleSubmit(event) {
     event.preventDefault();
     let user = document.getElementById('username');
@@ -153,7 +117,7 @@ let usernames = ["Andy", "Mike", "Rick", "Gigi", "Fede"];
 
 let form = document.getElementById(login-form);
 let errorMessage = document.getElementById('errors');
-
+/*
 function handleSubmit(event) {
     event.preventDefault();
 
@@ -169,3 +133,4 @@ function handleSubmit(event) {
         form.submit();
     }
 }
+*/
