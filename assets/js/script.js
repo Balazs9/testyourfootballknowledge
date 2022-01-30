37dfdb5a8da45c57bebf0ac58babbff04b7dd727
Balazs9 <sempergerbalazs@gmail.com>
2022-01-30 17:44:05 +0000

    nextQuestion function looping through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,21 @@ const questionBox = document.getElementById('questions');
 const resultsBox = document.getElementById('answer-buttons');
 const submitButton = document.getElementById('submit');
 const scoreCorrect = document.getElementById("score");
-const scoreWrong = document.getElementById("incoreect"); 
+const scoreWrong = document.getElementById("incorrect"); 
 const answerBox = document.getElementsByClassName("answ");
 const answer1 = document.getElementById("opt0");
 const answer2 = document.getElementById("opt1");
 const answer3 = document.getElementById("opt2");
 const answer4 = document.getElementById("opt3");
 const startingPage = 0;
+const questNumber = 4;
 
-
+/*
 answer1.onclick = function(event) {
     console.log(event);
+    if (answer1==question.correctAnwser) {
+        console.log(answ1);
+    };
 }
 answer2.onclick = function(event) {
     console.log(event);
@@ -24,48 +28,35 @@ answer4.onclick = function(event) {
     console.log(event);
 }
 
-submitButton.addEventListener("click", displayQuestion);
 
+submitButton.addEventListener("click", displayQuestion);
+*/
 /**
  * Displaying the quiz questions
  */
+
 let question = [
     {
-        quest: "Which team won the World Cup in 1938?",
-
-        answ1: "Italy",
-        answ2: "Germany",
-        answ3: "Spain",
-        answ4: "France",
+        questions: "Which team won the World Cup in 1938?",
+        
+        answers: ["Italy", "Germany", "Spain", "France"],
 
         correctAnswer: "Italy"
     },
     {
-        quest: "Which player won the Ballon D'or in 2003?",
+        questions: "Which player won the Ballon D'or in 2003?",
 
-        answ1: 'Pavel Nedved',
-        answ2: 'Zinadine Zidane',
-        answ3: 'Ronaldo',
-        answ4: 'Luis Figo',
+        answers: ['Pavel Nedved', 'Zinadine Zidane', 'Ronaldo', 'Luis Figo'],
 
         correctAnswer: "Pavel Nedved"
     }
 ]
-
-
-for(i=0; i<question.quest.length; i++) {
-    if(question[i].answers<question.answers.length){
-        console.log(question.answers);
-    };
-}
-
-function schuffleQuestion() {
-
-}
-
+/*
 function displayQuestion() {
     console.log("start");
-    questionBox.textContent = question[0].quest;
+    questionBox.textContent = question[q].questeions;
+
+
     answer1.textContent = question[0].answ1;
     answer2.textContent = question[0].answ2;
     answer3.textContent = question[0].answ3;
@@ -73,20 +64,29 @@ function displayQuestion() {
 }
 displayQuestion();
 
+
 function displayAnswers() {
     answerBox.textContent = question[0].answers;
 }
 displayAnswers();
+*/
+submitButton.addEventListener("click", nextQuestion);
 
 function nextQuestion() {
-    for (let question in question.quest.length) {
-        if (question <= question.quest.length) {
-            return question;
-        }
-        else {
-            return "game over";
+    console.log("begin");
+    for(var q in question){
+        console.log(question[q]);
+        for(var a in question[q].answers){
+            console.log("hello");
+            let correct=question[q].correct;
+            if(a==correct){
+                
+                console.log("good");
+            }
+            else {
+                console.log("wrong");
+            }
         }
     }
-};
+}
 nextQuestion();
-
