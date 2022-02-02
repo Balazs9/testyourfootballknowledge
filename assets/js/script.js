82a291b8fdeb3e88ef12f31cf1e532043e5e79d0
Balazs9 <sempergerbalazs@gmail.com>
2022-02-02 20:47:23 +0000

    fix error in correctanswer checking, to show the answer is correct or wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,27 +21,44 @@ let QuizBox = [
         
         answers: ["Italy", "Germany", "Spain", "France"],
 
-        correctAnswer: ["Italy"]
+        correctAnswer: "Italy"
     },
     {
         question: "Which player won the Ballon D'or in 2003?",
 
         answers: ["Pavel Nedved", "Zinadine Zidane", "Ronaldo", "Luis Figo"],
 
-        correctAnswer: ["Pavel Nedved"]
+        correctAnswer: "Pavel Nedved"
     },
     {
         question: "Where did Cristiano Ronaldo play at 2007?",
 
         answers: ["Real Madrid", "Sporting Lisboan", "Juventus", "Manchester United"],
 
-        correctAnswer: ["Manchester United"]
+        correctAnswer: "Manchester United"
+    },
+    {
+        question: "Who did not play with Kaka in Ac Milan in 2006? ",
+
+        answers: ["Clarance Seedorf", "Andrea Pirlo", "Filippo Inzaghi", "Ronaldinho"],
+
+        correctAnswer: "Ronaldinho"
+    },
+    {
+        question: "Which team won the Premier League in 2008? ",
+
+        answers: ["Chelsea", "Manchester City", "Mancheter United", "Arsenal"],
+
+        correctAnswer: "Manchester United"
+    },
+    {
+        question: "Which brand sponsored Marco Divaio? ",
+
+        answers: ["Puma", "Nike", "Adidas", "Mizuno"],
+
+        correctAnswer: "Nike"
     }
 ];
-let gameQuestion = [...QuizBox];
-
-let answer = gameQuestion[questionCount].correctAnswer;
-let choices = QuizBox[questionCount].answers;
 
 submitButton.addEventListener("click", beginGame);
 
@@ -56,12 +73,10 @@ function displayQuestion(questionCount) {
     questionBox.textContent = QuizBox[schuffleQuestion].question;
     for(let q=0; q<answerBox.length; q++){
         answerBox[q].textContent = QuizBox[schuffleQuestion].answers[q];
-       
+    
         answer1.onclick = function() {
-            if(answer==choices[0]){
+            if(QuizBox[schuffleQuestion].correctAnswer==QuizBox[schuffleQuestion].answers[0]){
                 console.log("good");
-                questionCount++;
-                next(questionCount);
             }
             else{
                 console.log("wrong");
@@ -69,7 +84,7 @@ function displayQuestion(questionCount) {
         };
         
         answer2.onclick = function() {
-            if(answer==choices[1]){
+            if(QuizBox[schuffleQuestion].correctAnswer==QuizBox[schuffleQuestion].answers[1]){
                 console.log("good");
             }
             else{
@@ -78,7 +93,7 @@ function displayQuestion(questionCount) {
         };
         
         answer3.onclick = function() {
-            if(answer==choices[2]){
+            if(QuizBox[schuffleQuestion].correctAnswer==QuizBox[schuffleQuestion].answers[2]){
                 console.log("good");
             }
             else{
@@ -87,7 +102,7 @@ function displayQuestion(questionCount) {
         };
         
         answer4.onclick = function() {
-            if(answer==choices[3]){
+            if(QuizBox[schuffleQuestion].correctAnswer==QuizBox[schuffleQuestion].answers[3]){
                 console.log("good");
             }
             else{
